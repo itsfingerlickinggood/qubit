499621f8238eb62373524736c640eb6221b22b7a
itsfingerlickinggood <sonuipad05@gmail.com>
2024-07-09 13:52:34 +0530

    analytics and speed

--- app/page.tsx
+++ app/page.tsx
@@ -23,11 +23,11 @@ export default async function Index() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <SpeedInsights />
+      <Analytics />
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-10xl flex justify-between items-center p-3 text-sm">
-          {isSupabaseConnected && <AuthButton /> && <SpeedInsights /> && (
-            <Analytics />
-          )}
+          {isSupabaseConnected && <AuthButton />}
           Hello World !
         </div>
       </nav>
